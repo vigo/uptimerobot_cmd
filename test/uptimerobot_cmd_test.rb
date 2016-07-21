2fe9b83a3c59c288b180763fefc9029db5750f1d
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-07-21 14:52:03 +0300

    test passes: test_get_alert_contacts_for_bilgi

--- test/uptimerobot_cmd_test.rb
+++ test/uptimerobot_cmd_test.rb
@@ -6,7 +6,7 @@ class UptimerobotCmdTest < Minitest::Test
   end
 
   def test_get_monitors
-    # skip "test this later"
+#    skip "test this later"
     monitors = ::UptimerobotCmd.get_monitors
 
     refute_empty monitors
@@ -16,12 +16,25 @@ class UptimerobotCmdTest < Minitest::Test
   end
   
   def test_get_alert_contacts
-    # skip "test this later"
+#    skip "test this later"
     contacts = ::UptimerobotCmd.get_alert_contacts
     refute_empty contacts
     assert_instance_of Array, contacts
     assert_equal true, contacts[0].has_key?('id')
     assert_equal true, contacts[0].has_key?('value')
     assert_equal true, contacts[0].has_key?('type')
+  end
+  
+  def test_get_alert_contacts_for_bilgi
+    skip "this test works with only with UPTIMEROBOT_BILGI_APIKEY" unless ENV['UPTIMEROBOT_BILGI_APIKEY']
+    contacts = ::UptimerobotCmd.get_alert_contacts
+    webteam_user = contacts.select{|c| c['friendlyname']  == 'webteam@bilgi.edu.tr'}.first
+    vigo_user = contacts.select{|c| c['friendlyname']  == 'vigo'}.first
+
+    assert_instance_of Hash, webteam_user
+    assert_instance_of Hash, vigo_user
+
+    assert_equal '0292994', webteam_user['id']
+    assert_equal '2412466', vigo_user['id']
   end
 end
