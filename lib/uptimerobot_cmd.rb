3cdaeb7faa7ecd13b4385447bc73cfe81934179f
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-07-21 13:53:32 +0300

    test passes: get_alert_contacts

--- lib/uptimerobot_cmd.rb
+++ lib/uptimerobot_cmd.rb
@@ -6,7 +6,8 @@ module UptimerobotCmd
   APIKEY = ENV['UPTIMEROBOT_APIKEY'] || nil
   ENDPOINT_BASE_URL = 'https://api.uptimerobot.com'
   ENDPOINT_SERVICE_URLS = {
-    get_monitors: '%{base_url}/getMonitors?apiKey=%{apikey}&format=json&noJsonCallback=1',
+    get_monitors: '%{base_url}/getMonitors?apiKey=%{apikey}&%{json_result}',
+    get_alert_contacts: '%{base_url}/getAlertContacts?apiKey=%{apikey}&%{json_result}',
   }
 
   class APIKEYError < StandardError; end
@@ -19,6 +20,7 @@ module UptimerobotCmd
   def self.build_service_url(service_key, **options)
     options[:apikey] = APIKEY
     options[:base_url] = ENDPOINT_BASE_URL
+    options[:json_result] = 'format=json&noJsonCallback=1'
     ENDPOINT_SERVICE_URLS[service_key] % options
   end
   
@@ -26,6 +28,13 @@ module UptimerobotCmd
     if self.apikey_defined
       response = HTTParty.get(build_service_url(:get_monitors))
       response["monitors"]["monitor"]
+    end
+  end
+  
+  def self.get_alert_contacts
+    if self.apikey_defined
+      response = HTTParty.get(build_service_url(:get_alert_contacts))
+      response["alertcontacts"]["alertcontact"]
     end
   end
 end
--- test/uptimerobot_cmd_test.rb
+++ test/uptimerobot_cmd_test.rb
@@ -4,13 +4,24 @@ class UptimerobotCmdTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::UptimerobotCmd::VERSION
   end
-    
+
   def test_get_monitors
+    # skip "test this later"
     monitors = ::UptimerobotCmd.get_monitors
 
     refute_empty monitors
     assert_instance_of Array, monitors
     assert_equal true, monitors[0].has_key?('id')
     assert_equal true, monitors[0].has_key?('url')
+  end
+  
+  def test_get_alert_contacts
+    # skip "test this later"
+    contacts = ::UptimerobotCmd.get_alert_contacts
+    refute_empty contacts
+    assert_instance_of Array, contacts
+    assert_equal true, contacts[0].has_key?('id')
+    assert_equal true, contacts[0].has_key?('value')
+    assert_equal true, contacts[0].has_key?('type')
   end
 end
